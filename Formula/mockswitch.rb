6c20af0243d0a927cca27339da207d8e2220d742
John Doe <hello@mockswitch.com>
2021-09-12 00:25:29 -0700

    mockswitch

--- Formula/mockswitch.rb
+++ Formula/mockswitch.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 class Espanso < Formula
-    desc "Cross-platform Text Expander written in Rust"
+    desc "Mock services simplified"
     homepage "https://mockswitch.com"
     url "https://github.com/mockswitch/homebrew-mockswitch/releases/download/1.29.0/mockswitch.tar.gz"
     sha256 "4763c8ceb7c3f369423a018fe8e2966ae6b57c3f089341f64668cfebba693f01"
